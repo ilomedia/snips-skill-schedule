3ace609404424bfccfe516dff5fa197f371b082d
ilomedia <contact@ilomedia.net>
2019-06-27 15:17:53 +0200

    test python3

--- action-getSchedule.py
+++ action-getSchedule.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ConfigParser
+import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
@@ -11,7 +11,7 @@ CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
 
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
+class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
@@ -22,7 +22,7 @@ def read_configuration_file(configuration_file):
             conf_parser = SnipsConfigParser()
             conf_parser.readfp(f)
             return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
+    except (IOError, configparser.Error) as e:
         return dict()
 
 
@@ -33,7 +33,7 @@ def subscribe_intent_callback(hermes, intent_message):
 
 def action_wrapper(hermes, intent_message, conf):
 
-    sentence = "ce matin, promenade au marché et à 17 heure cours de pilate sur la plage"
+    sentence = "ce matin, promenade au marché et a 17 heure cours de pilate sur la plage"
 
     hermes.publish_end_session(intent_message.session_id, sentence.decode("latin-1"))
 
