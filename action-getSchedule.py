b2921d2d158e68f00b1cda430218fe406e25de4b
ilomedia <contact@ilomedia.net>
2019-11-29 18:05:03 +0100

    cleanup des imports, threshold en float

--- action-getSchedule.py
+++ action-getSchedule.py
@@ -3,7 +3,6 @@
 
 import configparser
 from hermes_python.hermes import Hermes
-from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
 import io
 
@@ -11,7 +10,7 @@ CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
 
-class SnipsConfigParser(configparser.SafeConfigParser):
+class SnipsConfigParser(configparser.ConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
@@ -29,7 +28,7 @@ def read_configuration_file(configuration_file):
 def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
 
-    if intentMessage.asr_confidence < conf['global']['confidence_threshold']:
+    if intentMessage.asr_confidence < float(conf['global']['confidence_threshold']):
         hermes.publish_end_session(intentMessage.session_id)
     else:
         action_wrapper(hermes, intentMessage, conf)
